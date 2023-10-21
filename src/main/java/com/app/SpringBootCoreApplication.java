f4e40ddd869fc892319c8ff1855f7b2c3b008ec7
Ragubathy <ragubathy.b@gmail.com>
2023-10-21 19:57:36 +0800

    Update SpringBootCoreApplication.java

--- src/main/java/com/app/SpringBootCoreApplication.java
+++ src/main/java/com/app/SpringBootCoreApplication.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
 import java.io.PrintStream;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringBootCoreApplication {
@@ -17,7 +19,10 @@ public class SpringBootCoreApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SpringBootCoreApplication.class);
         app.setBannerMode(Banner.Mode.CONSOLE);
-        app.run(args);
+        ConfigurableApplicationContext ac = app.run(args);
+        Arrays.stream(ac.getBeanDefinitionNames()).forEach(System.out::println);
+        System.out.println("MyApplicationRunner bean ->"+ ac.getBean("myApplicationRunner"));
+        System.out.println("MyCommandLineRunner bean ->"+ ac.getBean("myCommandLineRunner"));
         System.out.println("Running Spring Core Application");
 
     }
